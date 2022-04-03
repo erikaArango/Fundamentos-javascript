e7a5a83849657a1e3bcfa8ff0933e2c00c0f3e3e
erikaArango <erika.arango@walmart.com>
2022-04-03 14:45:24 -0400

    gets y metododos estaticos

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -1,15 +1,37 @@
 
 
 class Persona {
+
+    static _conteo = 0;
+    static get conteo() {
+        return Persona._conteo + 'instancias';
+    }
+
+    static mensaje () {
+        console.log(this.nombre);
+        console.log('hola, soy un metodo estatico')
+    }
+
     nombre = '';
     codigo = '';
     frase = '';
+    comida = '';
 
     constructor( nombre = 'sin nombre', codigo = 'sin codigo', frase = 'sin frase') {
 
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
+
+        Persona._conteo++;
+    }
+
+    set setComidaFavorita (comida) {
+        this.comida = comida.toUpperCase();
+    }
+
+    get getComidaFavorita () {
+        return `la comida favorita de ${this.nombre} es ${this.comida}`
     }
 
     quienSoy() {
@@ -17,6 +39,7 @@ class Persona {
     }
 
     miFrase() {
+        this.quienSoy();
         console.log(`${this.codigo} dice : ${this.frase}`)
     }
 
@@ -25,8 +48,15 @@ class Persona {
 const spiderman = new Persona('peter parker','spider', 'soy amable');
 const ironman = new Persona('tony stark','ironman','yo soy ironman');
 
-console.log(spiderman);
 console.log(ironman);
 
 spiderman.miFrase();
-ironman.quienSoy();
+//ironman.quienSoy();
+
+spiderman.setComidaFavorita = 'el pie de cereza de la tia'
+
+// console.log(spiderman);
+// console.log(spiderman.getComidaFavorita);
+console.log('conteo estatico', Persona._conteo);
+console.log(Persona.conteo);
+Persona.mensaje();
