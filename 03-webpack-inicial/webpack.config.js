e82381913e611e3a7bc6414832e8084ea01193c2
erikaArango <erika.arango@walmart.com>
2022-04-05 19:43:59 -0400

    image management

--- 03-webpack-inicial/webpack.config.js
+++ 03-webpack-inicial/webpack.config.js
@@ -1,4 +1,5 @@
-const HtmlWebpack    = require('html-webpack-plugin')
+const HtmlWebpack          = require('html-webpack-plugin')
+const MiniCssExtract = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
@@ -18,8 +19,16 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                //exclude: /styles.css$/,
+                exclude: /styles.css$/,
                 use: [ 'style-loader', 'css-loader']
+            },
+            {
+                test: /styles.css$/,
+                use: [ MiniCssExtract.loader, 'css-loader' ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader'
             },
         ]
     },
@@ -31,6 +40,11 @@ module.exports = {
             title: 'Mi Webpack App',
             // filename: 'index.html',
             template: './src/index.html'
+        }),
+
+        new MiniCssExtract({
+            filename: '[name].css',
+            ignoreOrder: false
         }),
     ]
 }
